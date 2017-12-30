0d641456d709d052559bcf13852fa5a5803e3089
Dewey Dunnington <dewey@fishandwhistle.net>
2017-12-30 14:54:54 -0500

    make ec_climate_data_base work

--- R/climate_data.R
+++ R/climate_data.R
@@ -12,20 +12,60 @@
 #'   columns "date", "local_time", and "datetime", which is a timezone-aware POSIXct vector.
 #' @param check_dates Check dates with \link{ec_climate_locations_all} to avoid downloading empty files
 #' @param nice_names Use \link{nice_names} to make column names R-friendly
+#' @param endpoint The web address for the EC data service
 #'
 #' @return A data.frame (tibble)
 #' @export
 #'
 ec_climate_data <- function(location, timeframe = c("monthly", "daily", "hourly"),
-                         year = NULL, month = NULL, day = NULL, cache = "ec.cache", quiet = TRUE,
-                         parse_dates = TRUE, check_dates = TRUE, nice_names = TRUE) {
+                            year = NULL, month = NULL, day = NULL, cache = "ec.cache", quiet = TRUE,
+                            parse_dates = TRUE, check_dates = TRUE, nice_names = TRUE) {
+  # validate arguments
+  timeframe <- match.arg(timeframe)
 
+  # validate/resolve locations (ignore duplicates)
+  location <- as_ec_climate_location(unique(location))
+
+  # check year, month, and day args while generating arguments to pass to ec_climate_data_base()
+  if(timeframe == "monthly") {
+    # monthly data from EC requires no arguments
+    if(!is.null(day)) stop("Cannot get monthly data with a day constraint")
+    args <- tibble(location = location)
+  } else if(timeframe == "daily") {
+    # daily data from EC is downloaded with one file per year
+    if(is.null(year)) stop("Year required for daily requests")
+    args <- purrr::cross(list(location = location, year = unique(year)))
+  } else if(timeframe == "hourly") {
+    # hourly data from EC is donwloaded with one file per month
+    if(is.null(year)) stop("Year required for hourly requests")
+    if(is.null(month)) {
+      month <- 1:12
+    }
+    args <- purrr::cross(list(location = location, year = unique(year), month = unique(month)))
+  } else {
+    stop("Unrecognized timeframe: ", timeframe)
+  }
+
+  # warn the user if they are about to download lots of files, even if quiet = TRUE
+  if(nrow(args) > 1) message(sprintf("Downloading %s files (use quiet = FALSE for details)", nrow(args)))
+
+  # safely loop through each file
+  args$data <- lapply(purrr::transpose(args), function(row) try(do.call(ec_climate_data_base, row)))
+
+  args
 }
 
-ec_climate_data_base <- function(location, timeframe=c("monthly", "daily", "hourly"), year = NULL, month = NULL, day = NULL,
-                                 cache = "ec.cache", quiet = FALSE,
+#' @rdname ec_climate_data
+#' @export
+ec_climate_data_base <- function(location, timeframe=c("monthly", "daily", "hourly"),
+                                 year = NULL, month = NULL, day = NULL,
+                                 cache = NULL, quiet = FALSE,
                                  endpoint = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html") {
+  # validate arguments
   timeframe <- match.arg(timeframe)
+
+  # validate/resolve locations
+  location <- as_ec_climate_location(location)
 
   # make sure there is enough information (but not too much information) specified in the request
   ec_climate_check_timeframe_date(timeframe, year, month, day)
@@ -34,18 +74,21 @@ ec_climate_data_base <- function(location, timeframe=c("monthly", "daily", "hour
   x <- restquery(endpoint, .encoding="UTF-8",
                  format = "csv", stationID = as.numeric(location), submit = "Download Data",
                  timeframe = which(timeframe == c("hourly", "daily", "monthly")),
-                 Year = year, Month = month, cache = cache)
+                 Year = year, Month = month, .cache = cache)
 
   # NULL x is a failed download
   if(is.null(x)) stop("Download failed")
+
   # find how many lines are in the header
   xlines <- readr::read_lines(x)
   empty <- which(nchar(xlines) == 0)
   empty <- empty[empty != length(xlines)]
 
   # read the climate data (everything after the last empty line)
-  climate_data <- readr::read_csv(x, skip=empty[length(empty)],
-                                  col_types = readr::cols(.default = readr::col_character()))
+  # these files have partial lines occasionally, which causes readr::read_csv() to fail
+  climate_data <- utils::read.csv(textConnection(x), skip = empty[length(empty)],
+                                  stringsAsFactors = F, check.names = F,
+                                  colClasses = "character")
 
   # read the flag data if it exists (default is an empty table)
   flag_data <- tibble::tibble(flag = character(0), description = character(0))
@@ -69,6 +112,9 @@ ec_climate_data_base <- function(location, timeframe=c("monthly", "daily", "hour
     }
   }
 
+  # make climate_data a tibble
+  climate_data <- tibble::as_tibble(climate_data)
+
   # attach flag data to the climate data as an attribute
   attr(climate_data, "flag_info") <- flag_data
 
@@ -77,12 +123,12 @@ ec_climate_data_base <- function(location, timeframe=c("monthly", "daily", "hour
 }
 
 ec_climate_check_timeframe_date <- function(timeframe, year, month, day) {
-  if(timeframe == "daily" && is.na(year)) stop("Year required for daily requests")
-  if(timeframe == "hourly" && (is.na(year) || is.na(month) ))
+  if(timeframe == "daily" && is.null(year)) stop("Year required for daily requests")
+  if(timeframe == "hourly" && (is.null(year) || is.null(month) ))
     stop("Year and month required for hourly requests")
 
-  if(timeframe == "monthly" && (!is.na(year) || !is.na(month)))
-    stop("Specification of year/month not necessary for montly data")
-  if(timeframe == "daily" && (!is.na(month)))
+  if(timeframe == "monthly" && (!is.null(year) || !is.null(month)))
+    stop("Specification of year/month not necessary for monthly data")
+  if(timeframe == "daily" && (!is.null(month)))
     stop("Specification of month/day not necessary for daily data")
 }
---
+++ tests/testthat/test-climate_data.R
@@ -0,0 +1,20 @@
+context("climate_data")
+
+test_that("ec_climate_data_base works as intended", {
+  # check a single instance of each monthly, daily, and hourly files
+  monthly <- ec_climate_data_base(27141, timeframe = "monthly")
+  daily <- ec_climate_data_base(27141, timeframe = "daily", year = 1999)
+  hourly <- ec_climate_data_base(27141, timeframe = "hourly", year = 1999, month = 7)
+
+  expect_is(monthly, "tbl")
+  expect_is(daily, "tbl")
+  expect_is(hourly, "tbl")
+
+  monthly_flags <- attr(monthly, "flag_info")
+  daily_flags <- attr(daily, "flag_info")
+  hourly_flags <- attr(hourly, "flag_info")
+
+  expect_is(monthly_flags, "tbl")
+  expect_is(daily_flags, "tbl")
+  expect_is(hourly_flags, "tbl")
+})
